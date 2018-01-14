0df358f7932f1ef9eec44823f3a42991c1982e61
Denis Alves <dns_alves@hotmail.com>
2018-01-14 17:33:19 -0200

    projeto react sem mapa

--- src/index.js
+++ src/index.js
@@ -2,11 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Geocoder.fallbackToGoogle('AIzaSyAOVii1kLwTbTvCx-tQFZq0yOG-63J6lE0');
+
+class MapBox extends React.Component{
+
+	async getData(){
+		let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.props.locale.logradouro},${this.props.locale.localidade},${this.props.locale.uf}&key=AIzaSyAOVii1kLwTbTvCx-tQFZq0yOG-63J6lE0`
+		return await fetch(url)
+		.then((results) => { return results.json(); })
+		.then(data => {			
+			return data.results[0].geometry.location;
+		});
+
+	}
+
+	render(){
+  		let res = this.getData();
+		return (
+    		<div className="maps">
+    			<div lat={res} lng={this.props.locale.lng} ></div>
+  			</div>
+		)
+	}
+}
+
 class MapInfo extends React.Component{
+	constructor() {
+		super();
+
+		this._handleClick = this._handleClick.bind(this);
+	}
+ 
+  	_handleClick() {
+    	let mountNode = ReactDOM.findDOMNode(document.getElementById('map'));
+	    ReactDOM.unmountComponentAtNode(mountNode);
+  	}
 
 	render(){
 		return(
-			<div>{this.props.logradouro}</div>
+			<div className="col-sm-10 my-sm-2 maps-panel">
+            <button type="button" className="close" onClick={this._handleClick}>&times;</button>
+				<p className="logradouro">{this.props.locale.logradouro}</p>
+				<p>{this.props.locale.bairro}</p>
+				<p>{this.props.locale.localidade} - {this.props.locale.uf}</p>
+				<p>{this.props.locale.cep}</p>
+				<MapBox locale={this.props.locale} />
+			</div>
 		)
 	}
 }
@@ -14,43 +55,40 @@ class MapInfo extends React.Component{
 
 class SearchBox extends React.Component{
 
-
 	findAddress(){
 		fetch(`https://viacep.com.br/ws/${document.getElementById('CEP').value}/json/`)
 		.then((results) => { return results.json(); })
 		.then(data => {
-			return ReactDOM.render(<MapInfo logradouro={data.logradouro} />, document.getElementById('map'));
+			return ReactDOM.render(<MapInfo locale={data} />, document.getElementById('map'));
 		});
-	}
-
-	renderPanel() {
-		return (<Panel onClick={() => this.findAddress()} />); 
 	}
 
 	render(){
 		return(
-			<div className="background col-md-10">
+			<div className="background col-sm-10">
 				<h5>Consultar</h5>
 				<div className="form-inline">
 					<div className="form-group">
 						<label className="sr-only">CEP</label>
 						<p className="form-control-static">CEP</p>
 					</div>
-					<div className="form-group mx-md-2">
+					<div className="form-group mx-sm-2">
 						<label htmlFor="CEP" className="sr-only">Cep</label>
 						<input type="text" className="form-control" id="CEP" placeholder="03476-090" />
 					</div>
-					{this.renderPanel()}
+					<Button onClick={() => this.findAddress()} />
 				</div>
 			</div>
 		)
 	}
 }
 
-function Panel(props) {
-  return (<button type="submit" className="btn btn-primary" onClick={props.onClick}>Buscar</button>);
-}
+class Button extends React.Component {
 
+	render(){
+  		return (<button type="submit" className="btn btn-primary" onClick={this.props.onClick}>Buscar</button>);
+	}
+}
 
 ReactDOM.render(
   <SearchBox />,
